0fe8928908e8e1077907e9f099f741e4bf120363
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-04-29 16:23:34 -0400

    Add files via upload

--- MapsActivity.java
+++ MapsActivity.java
@@ -1,7 +1,6 @@
-package com.example.newyorkcarfare;
+package com.example.taxifarepredict;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
